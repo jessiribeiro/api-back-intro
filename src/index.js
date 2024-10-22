3465a547515064d7aa6b89fd1ca765e85024699d
Jéssica <jessiribeirob@gmail.com>
2024-10-21 21:57:19 -0300

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
 import express from "express";
+import cors from 'cors'; // Importa o pacote cors
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let usuarios = [];
 let id = 1; 
